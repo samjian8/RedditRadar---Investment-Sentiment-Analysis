c3a898be94734d56e9452d0cfa6562c7c4771652
samjian8 <samjian6289@gmail.com>
2025-09-06 12:26:45 -0400

    Initial commit: Reddit data fetching setup

---
+++ reddit_fetcher.py
@@ -0,0 +1,36 @@
+import praw
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+reddit = praw.Reddit(
+    client_id=os.getenv("CLIENT_ID"),
+    client_secret=os.getenv("CLIENT_SECRET"),
+    user_agent=os.getenv("USER_AGENT"),
+)
+
+
+def fetch_posts(subreddit: str, category: str, limit=10):
+
+    # Category can be one of the following: new, hot, top, rising, controversial, gilded
+    submissions = []
+    for submission in getattr(reddit.subreddit(subreddit), category)(limit=limit):
+        submissions.append({
+            "title": submission.title,
+            "score": submission.score,
+            "num_comments": submission.num_comments,
+            "created_utc": submission.created_utc,
+            "url": submission.url,
+            "upvote_ratio": submission.upvote_ratio,
+            "selftext": submission.selftext
+        })
+    return submissions
+
+submissions = fetch_posts("tifu", "new", 10)
+for submission in submissions:
+    title = submission["title"]
+    selftext = submission["selftext"]
+    print(title)
+    print(selftext)
+    print("-" * 100)
